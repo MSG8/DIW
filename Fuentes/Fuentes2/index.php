0626e95e7798b9490a55a9a0708f1d6a0b3352ad
MSG8 <masogo008@gmail.com>
2021-11-16 18:09:56 +0100

    Intento de trabajar con fuentes, tambein tengo que probar el audio y video

---
+++ Fuentes/Fuentes2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!-- Manuel Solís Gómez -->
+<HTML>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta type="author" content="Manuel Solis Gomez"/>
+		<meta type="description" content="Pagina para modificar informacion de empleados de una empresa"/>
+		<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=0.8, maximum-scale=2.0, minimum-scale=0.8"/>
+		<title> Administrador de empleados </title>
+		<link rel="stylesheet" type="text/css" href="css/trabajo.css">
+	</head>
+	<body>
+        <h1>ESTE ES UN TITULO BIEN BONITO</h1>
+        <H2>UN TIPO DE LETRA</H2>
+        <H2>OTRO TIPO DE LETRA</H2>
+        <H2>OTRO TIPO DE LETRA</H2>
+	</body>
+</HTML>
