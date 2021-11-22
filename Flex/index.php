6273d0bc8f739400c068bd1b2b208f23bb76c839
MSG8 <masogo008@gmail.com>
2021-11-22 09:25:07 +0100

    pruebas de flex y fuentes

---
+++ Flex/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!-- Manuel Solís Gómez -->
+<HTML>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta type="author" content="Manuel Solis Gomez"/>
+		<meta type="description" content="Pagina para modificar informacion de empleados de una empresa"/>
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1"/>
+		<title> Uso de flex </title>
+		<link rel="stylesheet" type="text/css" href="css/trabajo.css">
+	</head>
+	<body>
+        <nav>
+			<div>Manuel</div>
+			<div>Es</div>
+			<div>Good</div>
+		</nav>
+        <nav>
+			<div>Manuel</div>
+			<div>Es</div>
+			<div>Good</div>
+		</nav>
+        <nav>
+			<div>Manuel</div>
+			<div>Es</div>
+			<div>Good</div>
+		</nav>
+        <nav>
+			<div>Manuel</div>
+			<div>Es</div>
+			<div>Good</div>
+			<div>Manuel</div>
+			<div>Es</div>
+			<div>Good</div>
+			<div>Manuel</div>
+			<div>Es</div>
+			<div>Good</div>
+			<div>Manuel</div>
+			<div>Es</div>
+			<div>Good</div>
+			<div>Manuel</div>
+			<div>Es</div>
+			<div>Good</div>
+			<div>Manuel</div>
+		</nav>
+	</body>
+</HTML>
--- Fuentes/index.php
+++ Fuentes/index.php
@@ -7,12 +7,15 @@
 		<meta type="description" content="Pagina para modificar informacion de empleados de una empresa"/>
 		<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=0.8, maximum-scale=2.0, minimum-scale=0.8"/>
 		<title> Administrador de empleados </title>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Road+Rage&display=swap" rel="stylesheet"> <!--Para usar mas de una por html usaremos la & (family=fuente&family=fuente)-->
 		<link rel="stylesheet" type="text/css" href="css/trabajo.css">
 	</head>
 	<body>
-        <h1>ESTE ES UN TITULO BIEN BONITO</h1>
-        <H2>UN TIPO DE LETRA</H2>
-        <H2>OTRO TIPO DE LETRA</H2>
-        <H2>OTRO TIPO DE LETRA</H2>
+        <h1>FORMAS DE PONER LETRAS DIFERENTES</h1>
+        <h2>UN TIPO DE LETRA</h2>
+        <h2>OTRO TIPO DE LETRA</h2>
+        <h2>OTRO TIPO DE LETRA</h2>
 	</body>
 </HTML>
